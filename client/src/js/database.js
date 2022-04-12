6fe2aadb49d0a839a45aec25ae7f1633f4ab49a6
unknown <patticus.tv@gmail.com>
2022-04-12 16:46:44 +1000

    fully functional

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(content);
+  const request = store.put({ value: content});
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -35,7 +35,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
